cd35bb3589affc83f847293931778f098dfc5fde
setupishe <vasiliy.sokolov@nvi-solutions.com>
2025-01-13 22:40:25 +0300

    minor fixes

--- prepare_and_run_active.sh
+++ prepare_and_run_active.sh
@@ -1,2 +1,2 @@
-python3 prepare_al_split.py --from-fraction 0.4 --to-fraction 0.5 --weights /home/setupishe/ultralytics/runs/detect/active_0.4/weights/best.pt --conf 0.304 --cleanup
-yolo detect mode=train model=yolov8m.pt pretrained=False data=coco_0.5_active.yaml batch=48 name=active_0.5 epochs=65
+python3 prepare_al_split.py --from-fraction 0.6 --to-fraction 0.7 --weights /home/setupishe/ultralytics/runs/detect/active_0.6/weights/best.pt --conf 0.305 --cleanup
+yolo detect mode=train model=yolov8m.pt pretrained=False data=coco_0.7_active.yaml batch=48 name=active_0.7 epochs=65
