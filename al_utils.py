408d6974b69417dd683faf4d65b87517caef54d1
Vasiliy Sokolov <v.sokolov@visionlabs.ai>
2025-02-01 22:12:43 +0300

    fixed bug where density embeddings were chosen by largest value

--- al_utils.py
+++ al_utils.py
@@ -139,13 +139,14 @@ def select_embeddings(first_list, second_list, n=None, k=None, mode="distance"):
 
         embeds_with_distances.append((min_distance, file))
     # Extract the embeddings from the heap
-    sorted_embeds = sorted(embeds_with_distances, key=lambda x: x[0], reverse=True)
-
+    reverse = mode == "distance"
+    sorted_embeds = sorted(embeds_with_distances, key=lambda x: x[0], reverse=reverse)
     res = set()
     for pair in sorted_embeds:
-        name = os.path.basename(pair[1]).split("_")[0]
+        name = os.path.basename(pair[1])
+        img_name = name[: name.index("_cropped")]
         if len(res) < k:
-            res.add(name)
+            res.add(img_name)
         else:
             break
 
